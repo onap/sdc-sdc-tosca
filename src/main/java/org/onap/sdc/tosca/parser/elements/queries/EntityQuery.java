8fc529de36420a4fbb1553f04a52082d6fe9f474
Manzon, Inna (im453s) <im453s@intl.att.com>
2018-12-25 17:41:33 +0200

    SDC Tosca Parser getEntity API
    Change-Id: I4232c4ca85432bf9c81a81d717bee23b187182aa
    Issue-ID: SDC-1967
    Signed-off-by: Manzon, Inna (im453s) <im453s@intl.att.com>

--- src/main/java/org/onap/sdc/tosca/parser/elements/queries/EntityQuery.java
+++ src/main/java/org/onap/sdc/tosca/parser/elements/queries/EntityQuery.java
@@ -1,11 +1,16 @@
 package org.onap.sdc.tosca.parser.elements.queries;
 
 import org.apache.commons.lang3.StringUtils;
+import org.onap.sdc.tosca.parser.api.IEntityDetails;
 import org.onap.sdc.tosca.parser.enums.EntityTemplateType;
 import org.onap.sdc.tosca.parser.enums.SdcTypes;
-import org.onap.sdc.toscaparser.api.EntityTemplate;
+import org.onap.sdc.tosca.parser.impl.SdcPropertyNames;
+import org.onap.sdc.toscaparser.api.NodeTemplate;
+import org.onap.sdc.toscaparser.api.ToscaTemplate;
+import org.onap.sdc.toscaparser.api.elements.Metadata;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * This class describes an entity searched and retrieved by SDC Tosca Parser API
@@ -37,9 +42,9 @@ public abstract class EntityQuery {
         this.customizationUUID = customizationUUID;
     }
 
-    public abstract List<EntityTemplate> searchByTopologyTemplate(TopologyTemplateQuery topologyTemplateQuery);
+    public abstract List<IEntityDetails> getEntitiesFromTopologyTemplate(NodeTemplate nodeTemplate);
 
-    public abstract EntityTemplateType getType();
+    public abstract List<IEntityDetails> getEntitiesFromService(ToscaTemplate toscaTemplate);
 
     public EntityTemplateType getEntityType() {
         return entityType;
@@ -65,12 +70,33 @@ public abstract class EntityQuery {
         return StringUtils.isEmpty(toscaType) && nodeTemplateType == null;
     }
 
+    boolean isSearchCriteriaMatched(Metadata metadata, String toscaType, String uuidKeyName, String cuuidKeyName) {
+        return Objects.nonNull(metadata)
+                && isStringMatchingOrNull(metadata.getValue(uuidKeyName), getUUID())
+                && isStringMatchingOrNull(metadata.getValue(cuuidKeyName), getCustomizationUUID())
+                && isStringMatchingOrNull(toscaType, getToscaType());
+    }
+
+    boolean isSearchCriteriaMatched(Metadata metadata, String toscaType) {
+        return isSearchCriteriaMatched(metadata, toscaType, SdcPropertyNames.PROPERTY_NAME_UUID, SdcPropertyNames.PROPERTY_NAME_CUSTOMIZATIONUUID);
+    }
+
+    static boolean isStringMatchingOrNull(String currentUid, String uidInQuery) {
+        return uidInQuery == null || uidInQuery.equals(currentUid);
+    }
+
     public static EntityQueryBuilder newBuilder(EntityTemplateType entityTemplateType) {
         return new EntityQueryBuilder(entityTemplateType);
     }
 
     public static EntityQueryBuilder newBuilder(SdcTypes sdcType) {
         return new EntityQueryBuilder(sdcType);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("EntityType=%s, nodeTemplateType=%s, toscaType=%s, uUID=%s, customizationUUID=%s",
+                entityType, nodeTemplateType, toscaType, uUID, customizationUUID);
     }
 
     public static EntityQueryBuilder newBuilder(String toscaType) {
@@ -96,6 +122,9 @@ public abstract class EntityQuery {
                     break;
                 case POLICY:
                     entityQuery =  new PolicyEntityQuery();
+                    break;
+                case ALL:
+                    entityQuery = new AllEntitiesQuery();
                     break;
                 default:
                     throw new IllegalArgumentException("Wrong entity query type: " + entityTemplateType);
---
+++ src/test/java/org/onap/sdc/tosca/parser/elements/queries/TopologyTemplateQueryTest.java
@@ -0,0 +1,123 @@
+package org.onap.sdc.tosca.parser.elements.queries;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.onap.sdc.tosca.parser.enums.SdcTypes;
+import org.onap.sdc.tosca.parser.impl.SdcPropertyNames;
+import org.onap.sdc.toscaparser.api.NodeTemplate;
+import org.onap.sdc.toscaparser.api.elements.Metadata;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertFalse;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TopologyTemplateQueryTest {
+
+    @Mock
+    private Metadata metadata;
+
+    @Mock
+    private NodeTemplate nodeTemplate;
+
+    @Test(expected=IllegalArgumentException.class)
+    public void objectIsNotTopologyTemplate() {
+       TopologyTemplateQuery.newBuilder(SdcTypes.CP)
+               .build();
+    }
+
+    @Test
+    public void templateIsFoundByTypeOnly() {
+        TopologyTemplateQuery topologyTemplateQuery = TopologyTemplateQuery.newBuilder(SdcTypes.SERVICE)
+                .build();
+        when(nodeTemplate.getMetaData()).thenReturn(metadata);
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_CUSTOMIZATIONUUID)).thenReturn("345");
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_TYPE)).thenReturn(SdcTypes.SERVICE.getValue());
+        assertTrue(topologyTemplateQuery.isMatchingSearchCriteria(nodeTemplate));
+    }
+
+    @Test
+    public void templateIsNotFoundWhenMetadataIsNull() {
+        TopologyTemplateQuery topologyTemplateQuery = TopologyTemplateQuery.newBuilder(SdcTypes.VF)
+                .build();
+        when(nodeTemplate.getMetaData()).thenReturn(null);
+        assertFalse(topologyTemplateQuery.isMatchingSearchCriteria(nodeTemplate));
+    }
+
+    @Test
+    public void templateIsFoundIfItIsService() {
+        TopologyTemplateQuery topologyTemplateQuery = TopologyTemplateQuery.newBuilder(SdcTypes.SERVICE)
+                .build();
+        when(nodeTemplate.getMetaData()).thenReturn(metadata);
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_TYPE)).thenReturn(SdcTypes.SERVICE.getValue());
+        assertTrue(topologyTemplateQuery.isMatchingSearchCriteria(nodeTemplate));
+    }
+
+    @Test
+    public void templateIsFoundByTypeAndCUUID() {
+        TopologyTemplateQuery topologyTemplateQuery = TopologyTemplateQuery.newBuilder(SdcTypes.CVFC)
+                .customizationUUID("345")
+                .build();
+        when(nodeTemplate.getMetaData()).thenReturn(metadata);
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_TYPE)).thenReturn(SdcTypes.CVFC.getValue());
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_CUSTOMIZATIONUUID)).thenReturn("345");
+        assertTrue(topologyTemplateQuery.isMatchingSearchCriteria(nodeTemplate));
+    }
+
+    @Test
+    public void templateIsNotFoundWhenTypeIsNotMatchedAndCuuidIsNotSet() {
+        TopologyTemplateQuery topologyTemplateQuery = TopologyTemplateQuery.newBuilder(SdcTypes.CVFC)
+                .build();
+        when(nodeTemplate.getMetaData()).thenReturn(metadata);
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_TYPE)).thenReturn(SdcTypes.VF.getValue());
+        assertFalse(topologyTemplateQuery.isMatchingSearchCriteria(nodeTemplate));
+    }
+
+    @Test
+    public void templateIsFoundWhenTypeIsMatchedCuuidIsProvidedAndCuuidIsNullInMetadata() {
+        TopologyTemplateQuery topologyTemplateQuery = TopologyTemplateQuery.newBuilder(SdcTypes.VF)
+                .customizationUUID("2345")
+                .build();
+        when(nodeTemplate.getMetaData()).thenReturn(metadata);
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_CUSTOMIZATIONUUID)).thenReturn(null);
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_TYPE)).thenReturn(SdcTypes.VF.getValue());
+        assertFalse(topologyTemplateQuery.isMatchingSearchCriteria(nodeTemplate));
+    }
+
+    @Test
+    public void templateIsFoundWhenTypeIsMatchedAndCuuidIsNullInMetadata() {
+        TopologyTemplateQuery topologyTemplateQuery = TopologyTemplateQuery.newBuilder(SdcTypes.VF)
+                .build();
+        when(nodeTemplate.getMetaData()).thenReturn(metadata);
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_CUSTOMIZATIONUUID)).thenReturn(null);
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_TYPE)).thenReturn(SdcTypes.VF.getValue());
+        assertTrue(topologyTemplateQuery.isMatchingSearchCriteria(nodeTemplate));
+    }
+
+    @Test
+    public void templateIsNotFoundWhenTypeIsMatchedAndCuuidIsSet() {
+        TopologyTemplateQuery topologyTemplateQuery = TopologyTemplateQuery.newBuilder(SdcTypes.CVFC)
+                .customizationUUID("345")
+                .build();
+        when(nodeTemplate.getMetaData()).thenReturn(metadata);
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_TYPE)).thenReturn(SdcTypes.CVFC.getValue());
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_CUSTOMIZATIONUUID)).thenReturn("345");
+        assertTrue(topologyTemplateQuery.isMatchingSearchCriteria(nodeTemplate));
+    }
+
+    @Test
+    public void templateIsNotFoundWhenTypeIsNotMatchedAndCuuidIsSet() {
+        TopologyTemplateQuery topologyTemplateQuery = TopologyTemplateQuery.newBuilder(SdcTypes.CR)
+                .customizationUUID("345")
+                .build();
+        when(nodeTemplate.getMetaData()).thenReturn(metadata);
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_TYPE)).thenReturn(SdcTypes.CVFC.getValue());
+        when(metadata.getValue(SdcPropertyNames.PROPERTY_NAME_CUSTOMIZATIONUUID)).thenReturn("345");
+        assertFalse(topologyTemplateQuery.isMatchingSearchCriteria(nodeTemplate));
+    }
+
+
+}
