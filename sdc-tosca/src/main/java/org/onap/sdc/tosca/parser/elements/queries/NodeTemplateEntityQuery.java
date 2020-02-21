70b2d26fcf5159546fdd46868d15a7a9c8cc6a66
vasraz <vasyl.razinkov@est.tech>
2020-02-21 13:50:25 +0000

    Add support for simple yaml profile 1.2
    Fix NPE in GroupEntityQuery, NodeTemplateEntityQuery, PolicyEntityQuery, QueryProcessor
    Add new CSAR to test tosca_definitions_version: tosca_simple_yaml_1_2
    
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: Ib52616b3d86008d5fece2f146309b35d79fafa18
    Issue-ID: SDC-2738

--- sdc-tosca/src/main/java/org/onap/sdc/tosca/parser/elements/queries/NodeTemplateEntityQuery.java
+++ sdc-tosca/src/main/java/org/onap/sdc/tosca/parser/elements/queries/NodeTemplateEntityQuery.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,22 +21,23 @@
 package org.onap.sdc.tosca.parser.elements.queries;
 
 import com.google.common.collect.Lists;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.onap.sdc.tosca.parser.api.IEntityDetails;
 import org.onap.sdc.tosca.parser.elements.EntityDetailsFactory;
 import org.onap.sdc.tosca.parser.enums.EntityTemplateType;
 import org.onap.sdc.tosca.parser.enums.SdcTypes;
 import org.onap.sdc.tosca.parser.impl.SdcPropertyNames;
 import org.onap.sdc.toscaparser.api.NodeTemplate;
+import org.onap.sdc.toscaparser.api.SubstitutionMappings;
 import org.onap.sdc.toscaparser.api.ToscaTemplate;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Implements EntityQuery object for NodeTemplates
  */
 public class NodeTemplateEntityQuery extends EntityQuery {
+
     NodeTemplateEntityQuery() {
         super(EntityTemplateType.NODE_TEMPLATE, null, null);
     }
@@ -49,33 +50,39 @@ public class NodeTemplateEntityQuery extends EntityQuery {
         super(EntityTemplateType.NODE_TEMPLATE, null, toscaType);
     }
 
+    static List<IEntityDetails> convertNodeTemplatesListToEntityDetailsList(final Stream<NodeTemplate> nodeTemplates) {
+        return nodeTemplates
+            .map(nt -> EntityDetailsFactory.createEntityDetails(EntityTemplateType.NODE_TEMPLATE, nt))
+            .collect(Collectors.toList());
+    }
+
     @Override
-    public List<IEntityDetails> getEntitiesFromTopologyTemplate(NodeTemplate nodeTemplate) {
-        if (nodeTemplate.getSubMappingToscaTemplate() != null) {
-            return convertNodeTemplatesListToEntityDetailsList(filter(nodeTemplate.getSubMappingToscaTemplate()
-                    .getNodeTemplates()));
+    public List<IEntityDetails> getEntitiesFromTopologyTemplate(final NodeTemplate nodeTemplate) {
+        final SubstitutionMappings subMappingToscaTemplate = nodeTemplate.getSubMappingToscaTemplate();
+        if (subMappingToscaTemplate != null) {
+            final List<NodeTemplate> nodeTemplates = subMappingToscaTemplate.getNodeTemplates();
+            if (nodeTemplates != null) {
+                return convertNodeTemplatesListToEntityDetailsList(filter(nodeTemplates));
+            }
         }
         return Lists.newArrayList();
     }
 
     @Override
-    public List<IEntityDetails> getEntitiesFromService(ToscaTemplate toscaTemplate) {
-        return convertNodeTemplatesListToEntityDetailsList(filter(toscaTemplate.getNodeTemplates()));
+    public List<IEntityDetails> getEntitiesFromService(final ToscaTemplate toscaTemplate) {
+        final List<NodeTemplate> nodeTemplates = toscaTemplate.getNodeTemplates();
+        if (nodeTemplates != null) {
+            return convertNodeTemplatesListToEntityDetailsList(filter(nodeTemplates));
+        }
+        return Lists.newArrayList();
     }
 
-    static List<IEntityDetails> convertNodeTemplatesListToEntityDetailsList(Stream<NodeTemplate> nodeTemplates) {
-        return nodeTemplates
-            .map(nt->EntityDetailsFactory.createEntityDetails(EntityTemplateType.NODE_TEMPLATE, nt))
-            .collect(Collectors.toList());
-    }
-
-    private Stream<NodeTemplate> filter(List<NodeTemplate> nodeTemplateList) {
+    private Stream<NodeTemplate> filter(final List<NodeTemplate> nodeTemplateList) {
         return nodeTemplateList.stream()
-                .filter(nt->isSearchCriteriaMatched(nt.getMetaData(), nt.getType()))
-                .filter(nt->getNodeTemplateType() == null ||
-                        isStringMatchingOrNull(nt.getMetaData().getValue(SdcPropertyNames.PROPERTY_NAME_TYPE),
-                                getNodeTemplateType().getValue()));
+            .filter(nt -> isSearchCriteriaMatched(nt.getMetaData(), nt.getType()))
+            .filter(nt -> getNodeTemplateType() == null ||
+                isStringMatchingOrNull(nt.getMetaData().getValue(SdcPropertyNames.PROPERTY_NAME_TYPE),
+                    getNodeTemplateType().getValue()));
     }
-
 
 }
