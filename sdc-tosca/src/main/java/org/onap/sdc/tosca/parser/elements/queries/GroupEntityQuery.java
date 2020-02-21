7f0f1fd0a8cdbcc79a1ecfb8b5a80db054a68dd0
vasraz <vasyl.razinkov@est.tech>
2020-02-21 13:50:25 +0000

    Add support for simple yaml profile 1.2
    Fix NPE in GroupEntityQuery, NodeTemplateEntityQuery, PolicyEntityQuery, QueryProcessor
    Add new CSAR to test tosca_definitions_version: tosca_simple_yaml_1_2
    
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: Ib52616b3d86008d5fece2f146309b35d79fafa18
    Issue-ID: SDC-2738

--- sdc-tosca/src/main/java/org/onap/sdc/tosca/parser/elements/queries/GroupEntityQuery.java
+++ sdc-tosca/src/main/java/org/onap/sdc/tosca/parser/elements/queries/GroupEntityQuery.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,16 +21,17 @@
 package org.onap.sdc.tosca.parser.elements.queries;
 
 import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.onap.sdc.tosca.parser.api.IEntityDetails;
 import org.onap.sdc.tosca.parser.elements.EntityDetailsFactory;
 import org.onap.sdc.tosca.parser.enums.EntityTemplateType;
 import org.onap.sdc.toscaparser.api.Group;
 import org.onap.sdc.toscaparser.api.NodeTemplate;
+import org.onap.sdc.toscaparser.api.SubstitutionMappings;
 import org.onap.sdc.toscaparser.api.ToscaTemplate;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Implements EntityQuery object for Groups
@@ -44,32 +45,41 @@ public class GroupEntityQuery extends EntityQuery {
         super(EntityTemplateType.GROUP, null, null);
     }
 
+    GroupEntityQuery(String toscaType) {
+        super(EntityTemplateType.GROUP, null, toscaType);
+    }
+
+    static List<IEntityDetails> convertGroupLisToEntityDetailsList(final Stream<Group> groups) {
+        return groups.map(gr -> EntityDetailsFactory.createEntityDetails(EntityTemplateType.GROUP, gr))
+            .collect(Collectors.toList());
+    }
+
     @Override
-    public List<IEntityDetails> getEntitiesFromTopologyTemplate(NodeTemplate nodeTemplate) {
-        if (nodeTemplate.getSubMappingToscaTemplate() != null) {
-            return convertGroupLisToEntityDetailsList(filter(nodeTemplate.getSubMappingToscaTemplate().getGroups()));
+    public List<IEntityDetails> getEntitiesFromTopologyTemplate(final NodeTemplate nodeTemplate) {
+        final SubstitutionMappings subMappingToscaTemplate = nodeTemplate.getSubMappingToscaTemplate();
+        if (subMappingToscaTemplate != null) {
+            final List<Group> groups = subMappingToscaTemplate.getGroups();
+            if (groups != null) {
+                return convertGroupLisToEntityDetailsList(filter(groups));
+            }
         }
         return Lists.newArrayList();
     }
 
     @Override
-    public List<IEntityDetails> getEntitiesFromService(ToscaTemplate toscaTemplate) {
-        return convertGroupLisToEntityDetailsList(filter(toscaTemplate.getGroups()));
+    public List<IEntityDetails> getEntitiesFromService(final ToscaTemplate toscaTemplate) {
+        final List<Group> groups = toscaTemplate.getGroups();
+        if (groups != null) {
+            return convertGroupLisToEntityDetailsList(filter(groups));
+        } else {
+            return new ArrayList<>();
+        }
     }
 
-    GroupEntityQuery(String toscaType) {
-        super(EntityTemplateType.GROUP, null, toscaType);
-    }
-
-    static List<IEntityDetails> convertGroupLisToEntityDetailsList(Stream<Group> groups) {
-        return groups.map(gr->EntityDetailsFactory.createEntityDetails(EntityTemplateType.GROUP, gr))
-            .collect(Collectors.toList());
-    }
-
-    private Stream<Group> filter(List<Group> groupList) {
+    private Stream<Group> filter(final List<Group> groupList) {
         return groupList.stream()
-            .filter(gr->isSearchCriteriaMatched(gr.getMetadata(), gr.getType()) ||
-                    isSearchCriteriaMatched(gr.getMetadata(), gr.getType(), VF_MODULE_UUID, VF_MODULE_CUSTOMIZATION_UUID));
+            .filter(gr -> isSearchCriteriaMatched(gr.getMetadata(), gr.getType()) ||
+                isSearchCriteriaMatched(gr.getMetadata(), gr.getType(), VF_MODULE_UUID, VF_MODULE_CUSTOMIZATION_UUID));
     }
 
 }
