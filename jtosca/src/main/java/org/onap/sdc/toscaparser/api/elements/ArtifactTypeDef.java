a671e1ead04a2e1c4a21b8115fac539498d7690f
vasraz <vasyl.razinkov@est.tech>
2022-04-12 17:10:00 +0100

    Update tosca parser for node template aritfacts
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: I824f69a771fb17f3ad8562a96c3670d87158dc6e
    Issue-ID: SDC-3965

--- jtosca/src/main/java/org/onap/sdc/toscaparser/api/elements/ArtifactTypeDef.java
+++ jtosca/src/main/java/org/onap/sdc/toscaparser/api/elements/ArtifactTypeDef.java
@@ -21,14 +21,15 @@
 package org.onap.sdc.toscaparser.api.elements;
 
 import java.util.LinkedHashMap;
+import lombok.Getter;
 
+@Getter
 public class ArtifactTypeDef extends StatefulEntityType {
 
     private String type;
     private LinkedHashMap<String, Object> customDef;
     private LinkedHashMap<String, Object> properties;
     private LinkedHashMap<String, Object> parentArtifacts;
-
 
     public ArtifactTypeDef(String type, LinkedHashMap<String, Object> customDef) {
         super(type, ARTIFACT_PREFIX, customDef);
@@ -76,46 +77,4 @@ public class ArtifactTypeDef extends StatefulEntityType {
         return null;
     }
 
-    public String getType() {
-        return type;
-    }
-
 }
-
-/*python
-class ArtifactTypeDef(StatefulEntityType):
-    '''TOSCA built-in artifacts type.'''
-
-    def __init__(self, atype, custom_def=None):
-        super(ArtifactTypeDef, self).__init__(atype, self.ARTIFACT_PREFIX,
-                                              custom_def)
-        self.type = atype
-        self.custom_def = custom_def
-        self.properties = None
-        if self.PROPERTIES in self.defs:
-            self.properties = self.defs[self.PROPERTIES]
-        self.parent_artifacts = self._get_parent_artifacts()
-
-    def _get_parent_artifacts(self):
-        artifacts = {}
-        parent_artif = self.parent_type.type if self.parent_type else None
-        if parent_artif:
-            while parent_artif != 'tosca.artifacts.Root':
-                artifacts[parent_artif] = self.TOSCA_DEF[parent_artif]
-                parent_artif = artifacts[parent_artif]['derived_from']
-        return artifacts
-
-    @property
-    def parent_type(self):
-        '''Return a artifact entity from which this entity is derived.'''
-        if not hasattr(self, 'defs'):
-            return None
-        partifact_entity = self.derived_from(self.defs)
-        if partifact_entity:
-            return ArtifactTypeDef(partifact_entity, self.custom_def)
-
-    def get_artifact(self, name):
-        '''Return the definition of an artifact field by name.'''
-        if name in self.defs:
-            return self.defs[name]
-*/
---
+++ sdc-tosca/src/test/java/org/onap/sdc/impl/ToscaParserArtifactsTest.java
@@ -0,0 +1,87 @@
+/*
+ * -
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 Nordix Foundation.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.sdc.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.onap.sdc.tosca.parser.api.ISdcCsarHelper;
+import org.onap.sdc.tosca.parser.exceptions.SdcToscaParserException;
+import org.onap.sdc.tosca.parser.impl.SdcToscaParserFactory;
+import org.onap.sdc.toscaparser.api.NodeTemplate;
+import org.onap.sdc.toscaparser.api.elements.ArtifactTypeDef;
+
+class ToscaParserArtifactsTest {
+
+    private static final String TEST_ARTIFACTS_FILENAME = "csars/resource-VspWithArtifacts.csar";
+    private static final String TEST_ARTIFACT_TYPE = "tosca.artifacts.asd.deploymentItem";
+    private static final List<String> ARTIFACTS_NAME_LIST = Arrays.asList("sampleapp-db", "sampleapp-services");
+    private static ISdcCsarHelper helper = null;
+
+    @BeforeAll
+    public static void setup() {
+        try {
+            final URL resource = ToscaParserArtifactsTest.class.getClassLoader().getResource(TEST_ARTIFACTS_FILENAME);
+            if (resource != null) {
+                helper = SdcToscaParserFactory.getInstance().getSdcCsarHelper(resource.getFile());
+            }
+
+        } catch (final SdcToscaParserException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testGetArtifacts() {
+        assertNotNull(helper);
+        final NodeTemplate nodeTemplate = helper.getServiceNodeTemplates().get(0);
+        assertNotNull(nodeTemplate);
+        final Map<String, ArtifactTypeDef> artifacts = nodeTemplate.getArtifacts();
+        assertNotNull(artifacts);
+        assertEquals(2, artifacts.size());
+        artifacts.entrySet().forEach(entry -> {
+            assertTrue(ARTIFACTS_NAME_LIST.contains(entry.getKey()));
+            final ArtifactTypeDef artifactTypeDef = entry.getValue();
+            assertNotNull(artifactTypeDef);
+            final String artifactTypeDefType = artifactTypeDef.getType();
+            assertNotNull(artifactTypeDefType);
+            assertEquals(TEST_ARTIFACT_TYPE, artifactTypeDefType);
+            final LinkedHashMap<String, Object> properties = artifactTypeDef.getProperties();
+            assertNotNull(properties);
+            assertFalse(properties.isEmpty());
+            final String file = (String) artifactTypeDef.getDefinition("file");
+            assertNotNull(file);
+            assertTrue(file.startsWith("../Artifacts/Deployment/HELM/sampleapp-"));
+        });
+
+    }
+
+}
